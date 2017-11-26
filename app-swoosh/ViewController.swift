3cf18df005018c02b0b7f6f8ac382e33b7142d1b
Donald Belliveau <donsb@icloud.com>
2017-11-26 10:35:10 -0800

    Added Frames to my code for swoosh and image.

--- app-swoosh/ViewController.swift
+++ app-swoosh/ViewController.swift
@@ -9,10 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    /*
+     IBOutlets
+     */
+    @IBOutlet weak var swoosh: UIImageView!
+    @IBOutlet weak var BgImg: UIImageView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        /*
+         Add "Pixel Perfect".
+         This is controlling constraints from your code.
+         */
+        
+        swoosh.frame = CGRect(x: view.frame.size.width / 2 - swoosh.frame.size.width / 2, y: 50, width: swoosh.frame.size.width, height: swoosh.frame.size.height)
+        /*
+         View is the UIViewController's View.
+         frame is the position and size.
+         size is the size of the frame.
+         .width is the width of the screen.
+         The x code 1st divides by 2 to get the center of the screen (VIEW). The 2nd - the Swossh middle, to place swoosh in
+         the middle.  The first line alone would only put the top/left corner of Swoosh in the middle.
+         So, we place it in the middle, and then subtract the one half of swoosh so IT is in the middle.
+         */
+        
+        BgImg.frame = view.frame
+        // Make the image the exact size of the View.
     }
 
     override func didReceiveMemoryWarning() {
